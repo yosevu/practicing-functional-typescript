5b0f0667260e1aea818c40993ba6c7be53c2b7d5
Yosevu Kilonzo <yosevuk@gmail.com>
2019-10-10 10:00:56 -0400

    feat: add hello-world

---
+++ Exercism/javascript/hello-world/hello-world.js
@@ -0,0 +1,12 @@
+import { always } from 'ramda';
+
+/* Version 1 */
+
+// TODO: add type signature
+// const hello = () => 'Hello, World!';
+
+/* Version 2 */
+// TODO: add type signature
+const hello = always('Hello, World!');
+
+export { hello };
