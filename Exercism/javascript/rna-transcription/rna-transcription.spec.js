d83ee8ff449c9c46231353fb1abf5b481fb6d7cb
Yosevu Kilonzo <yosevuk@gmail.com>
2019-10-10 10:09:46 -0400

    feat: add rna-transcription

---
+++ Exercism/javascript/rna-transcription/rna-transcription.spec.js
@@ -0,0 +1,27 @@
+import { toRna } from './rna-transcription'
+
+describe('Transcription', () => {
+  test('empty rna sequence', () => {
+    expect(toRna('')).toEqual('');
+  });
+
+  test('transcribes cytosine to guanine', () => {
+    expect(toRna('C')).toEqual('G');
+  });
+
+  test('transcribes guanine to cytosine', () => {
+    expect(toRna('G')).toEqual('C');
+  });
+
+  test('transcribes thymine to adenine', () => {
+    expect(toRna('T')).toEqual('A');
+  });
+
+  test('transcribes adenine to uracil', () => {
+    expect(toRna('A')).toEqual('U');
+  });
+
+  test('transcribes all dna nucleotides to their rna complements', () => {
+    expect(toRna('ACGTGGTCTTAA')).toEqual('UGCACCAGAAUU');
+  });
+})
