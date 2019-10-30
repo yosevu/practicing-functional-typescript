e3496063d7f2844cbde4f1403c7176d9f9cf2d68
Yosevu Kilonzo <yosevuk@gmail.com>
2019-10-29 22:20:15 -0400

    feat: set up project
    Set up project and dependencies

---
+++ src/exercises/two-fer/two-fer.spec.js
@@ -0,0 +1,15 @@
+import { twoFer } from './two-fer'
+
+describe('twoFer()', () => {
+  test('no name given', () => {
+    expect(twoFer()).toEqual("One for you, one for me.")
+  })
+
+  xtest('a name given', () => {
+    expect(twoFer("Alice")).toEqual("One for Alice, one for me.")
+  })
+
+  xtest('another name given', () => {
+    expect(twoFer("Bob")).toEqual("One for Bob, one for me.")
+  })
+})
