45cff75e0de20a094757d905a1eb16c3731939e0
Yosevu Kilonzo <yosevuk@gmail.com>
2019-11-04 08:59:58 -0500

    feat: add gigasecond

---
+++ src/solutions/gigasecond/gigasecond.js
@@ -0,0 +1,13 @@
+// import { pipe } from 'ramda';
+
+/* Version 1 */
+
+const gigasecond = utcDate => {
+  return new Date(Number(utcDate) + 10E11);
+};
+
+/* Version 2 */
+
+export {
+  gigasecond
+};
