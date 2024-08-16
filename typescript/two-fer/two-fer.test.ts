861e08b305b5837afb7052297c14d02b6188d2e2
Yosevu Kilonzo <yosevu@yosevu.com>
2024-08-15 22:29:19 -0400

    feat: add two-fer

---
+++ typescript/two-fer/two-fer.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect, xit } from '@jest/globals'
+import { twoFer } from './two-fer.ts'
+
+describe('TwoFer', () => {
+  it('no name given', () => {
+    const expected = 'One for you, one for me.'
+    expect(twoFer()).toEqual(expected)
+  })
+
+  xit('a name given', () => {
+    const expected = 'One for Alice, one for me.'
+    expect(twoFer('Alice')).toEqual(expected)
+  })
+
+  xit('another name given', () => {
+    const expected = 'One for Bob, one for me.'
+    expect(twoFer('Bob')).toEqual(expected)
+  })
+})
